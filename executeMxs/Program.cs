96208c651ce964c9915380e6417bd6f2a0b49881
Panteleev Sergey <fatbbc@gmail.com>
2019-02-24 13:03:43 +0300

    added project

---
+++ executeMxs/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace executeMxs
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            // args[0] : custom user string
+            // args[1] : 3dsmax mxseditor active document FilePath
+
+
+            if ( args.Length < 2 ) return;
+
+            // first check that any 3dsmax instance is running
+            var processes = Process.GetProcessesByName( "3dsmax" );
+
+            if ( processes.Length == 0 ) return;
+
+
+
+
+            // now we need to find all mxseditor windows
+            var mxseditors = WinAPI.FilterByClassName( WinAPI.GetChildWindows( new IntPtr( 0 ) ), "MXS_SciTEWindow" );
+
+
+            uint PID = 0;
+            string mxseWindowTitle = (string)args.GetValue( 1 );
+
+            // compare passed argument string with mxse window titles and if it matches check process ID to identify correct 3dsmax instance
+            foreach ( var mxse in mxseditors )
+            {
+                if ( WinAPI.GetWindowText( mxse ).StartsWith( mxseWindowTitle ) )
+                {
+
+                    WinAPI.GetWindowThreadProcessId( mxse, out PID );
+                    break;
+                }
+
+            }
+
+            var active3dsMaxProcess = processes.Where( proc => proc.Id == PID ).ToList();
+
+
+            if ( active3dsMaxProcess.Count == 0 )
+            {
+                // in case when script isn't saved check process ID of active window
+
+                IntPtr activeWindow = WinAPI.GetForegroundWindow();
+                WinAPI.GetWindowThreadProcessId( activeWindow, out PID );
+
+                active3dsMaxProcess = processes.Where( proc => proc.Id == PID ).ToList();
+
+                if ( active3dsMaxProcess.Count == 0 )
+                {
+                    MessageBox.Show( "Parent process not found." );
+                    return;
+                }
+            }
+
+
+
+            var minilistener = WinAPI.FilterByClassName( WinAPI.GetChildWindows( active3dsMaxProcess[ 0 ].MainWindowHandle ), "MXS_Scintilla" );
+
+
+            if ( minilistener.Count == 0 )
+            {
+                MessageBox.Show( "No windows found" );
+                return;
+            }
+
+
+            int WM_CHAR = 0x0102;
+            int VK_RETURN = 0x0D;
+            int WM_SETTEXT = 0x000C;
+
+
+            string src = string.Format( "{0}();", (string)args.GetValue( 0 ) );
+
+
+            WinAPI.SendMessage( minilistener[ 1 ], WM_SETTEXT, src.Length + 1, src );
+            WinAPI.PostMessage( minilistener[ 1 ], WM_CHAR, VK_RETURN, 0 );
+
+
+
+        }
+    }
+}
---
+++ executeMxs/WinAPI.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace executeMxs
+{
+    static class WinAPI
+    {
+
+        [DllImport( "user32" )]
+        [return: MarshalAs( UnmanagedType.Bool )]
+        private static extern bool EnumChildWindows( IntPtr window, EnumWindowProc callback, IntPtr i );
+
+        /// <summary>
+        /// Returns a list of child windows
+        /// </summary>
+        /// <param name="parent">Parent of the windows to return</param>
+        /// <returns>List of child windows</returns>
+        public static List<IntPtr> GetChildWindows( IntPtr parent )
+        {
+            List<IntPtr> result = new List<IntPtr>();
+            GCHandle listHandle = GCHandle.Alloc( result );
+            try
+            {
+                EnumWindowProc childProc = new EnumWindowProc( EnumWindow );
+                EnumChildWindows( parent, childProc, GCHandle.ToIntPtr( listHandle ) );
+            }
+            finally
+            {
+                if ( listHandle.IsAllocated )
+                    listHandle.Free();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Callback method to be used when enumerating windows.
+        /// </summary>
+        /// <param name="handle">Handle of the next window</param>
+        /// <param name="pointer">Pointer to a GCHandle that holds a reference to the list to fill</param>
+        /// <returns>True to continue the enumeration, false to bail</returns>
+        private static bool EnumWindow( IntPtr handle, IntPtr pointer )
+        {
+            GCHandle gch = GCHandle.FromIntPtr( pointer );
+            List<IntPtr> list = gch.Target as List<IntPtr>;
+            if ( list == null )
+            {
+                throw new InvalidCastException( "GCHandle Target could not be cast as List<IntPtr>" );
+            }
+            list.Add( handle );
+            //  You can modify this to check to see if you want to cancel the operation, then return a null here
+            return true;
+        }
+
+        /// <summary>
+        /// Delegate for the EnumChildWindows method
+        /// </summary>
+        /// <param name="hWnd">Window handle</param>
+        /// <param name="parameter">Caller-defined variable; we use it for a pointer to our list</param>
+        /// <returns>True to continue enumerating, false to bail.</returns>
+        public delegate bool EnumWindowProc( IntPtr hWnd, IntPtr parameter );
+
+        
+        public static List<IntPtr> FilterByClassName( List<IntPtr> ptrs, string className )
+        {
+            return ptrs.Where( ptr =>
+            {
+
+                StringBuilder ClassNameSB = new StringBuilder( 256 );
+                int nRet = GetClassName( ptr, ClassNameSB, ClassNameSB.Capacity );
+                if ( nRet != 0 )
+                {
+                    return string.Compare( ClassNameSB.ToString(), className, true, CultureInfo.InvariantCulture ) == 0;
+
+                }
+
+                return false;
+            }
+
+            ).ToList();
+
+        }
+
+
+
+
+
+
+
+        public static string GetWindowText( IntPtr hWnd )
+        {
+            int len = GetWindowTextLength( hWnd ) + 1;
+            StringBuilder sb = new StringBuilder( len );
+            len = GetWindowText( hWnd, sb, len );
+            return sb.ToString( 0, len );
+        }
+
+        [DllImport( "user32.dll", SetLastError = true )]
+        static extern int GetWindowTextLength( IntPtr hWnd );
+        [DllImport( "user32.dll", SetLastError = true )]
+        static extern int GetWindowText( IntPtr hWnd, StringBuilder lpString, int nMaxCount );                
+
+        [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+        public static extern bool SetWindowText( IntPtr hwnd, String lpString );
+
+        [DllImport( "user32.dll", CharSet = CharSet.Auto, SetLastError = true )]
+        public static extern int SendMessage( IntPtr hWnd, int wMsg, int wParam, string lParam );
+        [DllImport( "user32.dll", CharSet = CharSet.Auto, SetLastError = true )]
+        public static extern int PostMessage( IntPtr hWnd, int wMsg, int wParam, [MarshalAs( UnmanagedType.LPStr )] string lParam );
+
+
+
+        [DllImport( "user32.dll" )]
+        public static extern int PostMessage( IntPtr hWnd, int wMsg, long wParam, long lParam );
+
+        [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+        static extern int GetClassName( IntPtr hWnd, StringBuilder lpClassName, int nMaxCount );
+
+        [DllImport( "user32.dll", SetLastError = true )]
+        public static extern uint GetWindowThreadProcessId( IntPtr hWnd, out uint processId );
+
+        [DllImport( "user32.dll" )]
+        public static extern bool SetForegroundWindow( IntPtr hWnd );
+        [DllImport( "user32.dll" )]
+        public static extern IntPtr GetForegroundWindow();
+
+
+    }
+}
